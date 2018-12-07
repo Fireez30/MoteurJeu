b604d2cfb7bd37740030a5d42a13174e6e75630a
Foopyo <romain.chamoulaud@gmail.com>
2018-12-07 12:53:57 +0100

    Ajout de mes paths

--- gamemanager.cpp
+++ gamemanager.cpp
@@ -230,9 +230,10 @@ void attributeRoom(int** minMap, std::vector<Rooms>* rooms, std::string path){
 
 void GameManager::initializeGL()
 {
-    std::string path = "C:\\Users\\Fireez\\Documents\\GitHub\\MoteurJeu\\Rooms";
+    std::string path = "D:\\Git\\MoteurJeu\\Rooms";
     //"D:\\Enseignement\\Moteur de jeux\\TP\\MoteurHere\\MoteurJeu\\Rooms"; - Benj portable
     //"C:\\Users\\Fireez\\Documents\\GitHub\\MoteurJeu\\Rooms" - Benj fixe
+    //"D:\\Git\\MoteurJeu\\Rooms" - Romain portable
     initializeOpenGLFunctions();
 
     glClearColor(0,0,0, 1);
@@ -343,9 +344,11 @@ void GameManager::initShaders()
 void GameManager::initTextures()
 {
     QImage img;
-    std::string s = "C:\\Users\\Fireez\\Documents\\GitHub\\MoteurJeu\\sprites.png";
+    std::string s = "D:\\Git\\MoteurJeu\\sprites.png";
     //"D:\\Enseignement\\Moteur de jeux\\TP\\MoteurHere\\MoteurJeu\\sprites.png"; - Benj portable
     //"C:\\Users\\Fireez\\Documents\\GitHub\\MoteurJeu\\sprites.png" - Benj fixe
+    //"D:\\Git\\MoteurJeu\\sprites.png" - Romain portable
+
     img.load(s.data());
     texture = new QOpenGLTexture(img); //chargement de la sprite sheet ici
     // Set nearest filtering mode for texture minification
